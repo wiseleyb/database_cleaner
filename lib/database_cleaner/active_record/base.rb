f9cb3b39017dc356b66b160c07a35c7d0e6db9a2
wiseleyb <wiseleyb@rasputin.local>
2010-11-23 14:05:19 -0800

    adding :none to cleaner options

--- lib/database_cleaner/active_record/base.rb
+++ lib/database_cleaner/active_record/base.rb
@@ -5,7 +5,7 @@ module DatabaseCleaner
   module ActiveRecord
 
     def self.available_strategies
-      %w[truncation transaction deletion]
+      %w[truncation transaction deletion none]
     end
 
     def self.config_file_location
